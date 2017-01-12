3f52be2b142c472ac4a6738c496130e11a2a432b
Training4 <training@coremedia.com>
2017-01-12 15:23:55 +0100

    Exercise 4+5

--- studio-plugins/coredining-studio-plugin/src/main/resources/META-INF/resources/joo/coredining-studio-plugin.module.js
+++ studio-plugins/coredining-studio-plugin/src/main/resources/META-INF/resources/joo/coredining-studio-plugin.module.js
@@ -1,4 +1,5 @@
 joo.loadModule("${project.groupId}", "${project.artifactId}");
+joo.loadStyleSheet("coredining-theme/css/coredining-doctype-icons.css")
 coremediaEditorPlugins.push({
   name:"CoreDining Studio Plugin",
   mainClass:"com.coremedia.coredining.studio.CoreDiningStudioPlugin"
